e89891eb303cc9a0cef08561b3c8bdba11b20b89
jjakimoto <paul.of.tpp@gmail.com>
2016-10-15 02:09:57 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 import scipy.misc
 import numpy as np
+import keras
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-from config import DCGANconfig 
+# from config import DCGANconfig 
+from conig import mnistConfig
 from model import DCGAN
 from utils import get_images
 
-import keras
-import tensorflow as tf
-
 def main(data_path):
-    # config = DCGANconfig()
+    # config = DCGANConfig()
     config = mnistConfig()
     dcgan = DCGAN(config)
-    data = get_data(data_path)
+    # data = get_images(data_path)
+    data = mnist.train.images.reshape([-1, 28, 28])
     dcgan.train(data)
 
-
+if __name__ = '__main__':
+    main()
